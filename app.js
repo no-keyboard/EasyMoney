0a496cbd3f21a8b1a70d66745bf59fa694d505d8
no-keyboard <finaldestiny92@gmail.com>
2024-12-07 23:52:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, '/index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
 
 const server = app.listen(port, () => console.log(`Listening on port ${port}!`));
 
